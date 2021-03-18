d27e48d4d6b25d1437a48ae0df2b75c7b07b1ed7
k-ken-t4g <kento.k.92@gmail.com>
2021-03-18 09:54:09 +0900

    Erased required script by mistake

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+get_distribution() {
+	lsb_dist=""
+	# Every system that we officially support has /etc/os-release
+	if [ -r /etc/os-release ]; then
+		lsb_dist="$(. /etc/os-release && echo "$ID")"
+	fi
+	# Returning an empty string here should be alright since the
+	# case statements don't act unless you provide an actual value
+	echo "$lsb_dist"
+}
+
+lsb_dist=$( get_distribution )
+lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
+
 sudo apt-get update
 sudo apt upgrade -y
 
